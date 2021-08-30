89736d7bb9649e4ba0f065cba292dfd97285405f
Rushiraj11 <grushiraj95@gmail.com>
2021-08-30 09:38:35 +0530

    Added: Assigned Player Options

--- src/main/java/com/BridgeLabz/TicTacToe.java
+++ src/main/java/com/BridgeLabz/TicTacToe.java
@@ -1,10 +1,11 @@
 package com.BridgeLabz;
-
+import java.util.Scanner;
 public class TicTacToe {
     public static final char[] boardarray = new char[10];
 
     public static void main(String[] args) {
-   createBoard();
+        createBoard();
+        PlayerOption();
     }
 
     public static void createBoard() {
@@ -13,6 +14,18 @@ public class TicTacToe {
         for (i = 1; i < boardarray.length; i++) {
             boardarray[i] = ' ';
 
+        }
+    }
+
+    public static void PlayerOption() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Player One: Choose X or O =");
+        char playerChoose = sc.next().charAt(0);
+        System.out.println(playerChoose);
+        if (playerChoose == 'X') {
+            System.out.println("Player Selected X");
+        } else {
+            System.out.println("CPU Selected O");
         }
     }
 }
